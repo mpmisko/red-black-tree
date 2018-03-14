0c4beed21e57ab3f7d379a813fdeda630f451882
mp5217 <michal.pandy17@imperial.ac.uk>
2018-03-14 22:09:03 +0000

    correct type

--- src/redblacktree/Main.java
+++ src/redblacktree/Main.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class Main {
 
     private static final int MAX_SIZE = 500;
-    private static final int REPITITIONS = 500;
+    private static final int REPETITIONS = 500;
 
     public static void main(String[] args) throws FileNotFoundException {
 
